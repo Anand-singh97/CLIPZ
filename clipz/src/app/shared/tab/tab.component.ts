e6996482b3f07dc19a13343be2558fde1b9a2f2d
Anandpravesh Singh <Asingh4337@conestogac.on.ca>
2024-03-22 16:56:14 -0400

    Tabs Component is completed

---
+++ clipz/src/app/shared/tab/tab.component.ts
@@ -0,0 +1,14 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-tab',
+  templateUrl: './tab.component.html',
+  styleUrl: './tab.component.css'
+})
+export class TabComponent {
+
+  @Input() tabTitle: string = '';
+  @Input() active: boolean = false;
+  constructor() {
+  }
+}
--- clipz/src/app/shared/tabs-container/tabs-container.component.ts
+++ clipz/src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, ContentChildren, AfterContentInit, QueryList} from '@angular/core';
+import {TabComponent} from "../tab/tab.component";
 
 @Component({
   selector: 'app-tabs-container',
   templateUrl: './tabs-container.component.html',
   styleUrl: './tabs-container.component.css'
 })
-export class TabsContainerComponent {
+export class TabsContainerComponent implements AfterContentInit
+{
+  @ContentChildren(TabComponent) tabs?: QueryList<TabComponent>;
 
+  constructor() {
+  }
+
+  ngAfterContentInit(): void {
+    const activeTabs = this.tabs?.filter((tab: TabComponent): void=>{
+      tab.active;
+    });
+    if(!activeTabs || activeTabs.length === 0)
+    {
+      this.selectTab(this.tabs?.first);
+    }
+  }
+
+  selectTab(tab?: TabComponent, e?: Event): void{
+
+    e?.preventDefault();
+    this.tabs?.forEach((t: TabComponent):void =>{
+      t.active = false;
+    })
+    tab!.active = true;
+  }
 }
